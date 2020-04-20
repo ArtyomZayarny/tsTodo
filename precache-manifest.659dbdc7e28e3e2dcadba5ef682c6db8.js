96390ed219599a78bcdac621bb3d485a80f1d876
artem <artyom.zayarny@gmail.com>
2020-04-20 11:24:50 +0300

    Updates

---
+++ precache-manifest.659dbdc7e28e3e2dcadba5ef682c6db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60a116f7f5c261052091b14fccb361a",
+    "url": "/tsTodo/index.html"
+  },
+  {
+    "revision": "620da57cd9f5bed98267",
+    "url": "/tsTodo/static/css/main.967db44a.chunk.css"
+  },
+  {
+    "revision": "b5790e6a832dba149002",
+    "url": "/tsTodo/static/js/2.486d3384.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tsTodo/static/js/2.486d3384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620da57cd9f5bed98267",
+    "url": "/tsTodo/static/js/main.29ffd109.chunk.js"
+  },
+  {
+    "revision": "508e49d54b1c50bf8389",
+    "url": "/tsTodo/static/js/runtime-main.70a28427.js"
+  }
+]);
